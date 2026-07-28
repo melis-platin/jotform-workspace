df0fa75207b836abc8fc45200dfea03fd2979eea
melis-platin <melisplatin@gmail.com>
2026-07-28 09:23:08 +0300

    Make icon-only WhatsApp buttons square

--- packages/app-elements/src/components/WhatsApp/WhatsApp.tsx
+++ packages/app-elements/src/components/WhatsApp/WhatsApp.tsx
@@ -55,6 +55,7 @@ export const WhatsApp: FC<WhatsAppProps> = ({
     'jf-whatsapp',
     !isButtonStyle && !showLabel && 'jf-whatsapp--no-label',
     !isButtonStyle && showLabel && `jf-whatsapp--bubble-${bubblePlacement.toLowerCase()}`,
+    isButtonStyle && !isButtonIconAndText && 'jf-whatsapp--button-icon-only',
     isButtonStyle
       ? `jf-whatsapp--button jf-whatsapp--button-size-${size.toLowerCase()} jf-whatsapp--button-width-${buttonWidth.toLowerCase()} jf-whatsapp--button-align-${buttonAlignment.toLowerCase()}`
       : `jf-whatsapp--size-${size.toLowerCase()} jf-whatsapp--align-${alignment.toLowerCase()}`,
